31eada318b77e7e1137919a2585edb0acaa2a1c2
Vishal Sharma <141960317+vishalsharma7905@users.noreply.github.com>
2026-01-20 12:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.34%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def postorderTraversal(self, root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            dfs(node.right)
+            result.append(node.val)
+
+        dfs(root)
+        return result
