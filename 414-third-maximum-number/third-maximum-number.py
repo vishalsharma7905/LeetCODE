d90e3d5f808271452df58f881d84f16030413215
Vishal Sharma <141960317+vishalsharma7905@users.noreply.github.com>
2026-04-10 14:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (69.32%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,18 +1,21 @@
 class Solution:
-    def thirdMax(self, nums):
+    def thirdMax(self, nums: List[int]) -> int:
         first = second = third = None
-
-        for n in nums:
-            if n == first or n == second or n == third:
+        
+        for num in nums:
+            if num == first or num == second or num == third:
                 continue
-            if first is None or n > first:
+            
+            if first is None or num > first:
                 third = second
                 second = first
-                first = n
-            elif second is None or n > second:
+                first = num
+                
+            elif second is None or num > second:
                 third = second
-                second = n
-            elif third is None or n > third:
-                third = n
-
+                second = num
+                
+            elif third is None or num > third:
+                third = num
+        
         return third if third is not None else first
