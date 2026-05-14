c8fd1280859ca475736bdacd25855ccdd6147dfd
Vishal Sharma <141960317+vishalsharma7905@users.noreply.github.com>
2026-05-15 00:16:50 +0530

    Time: 47 ms (23.84%) | Memory: 31.5 MB (20.24%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = nums[0]
+        max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
